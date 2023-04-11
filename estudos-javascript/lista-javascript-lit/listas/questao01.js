531d06d810739218bc7fbc795a997ba1574b39c2
Nycole <62551765+nycolemendonca@users.noreply.github.com>
2023-04-11 14:47:57 -0300

    Update questao01.js

--- estudos-javascript/lista-javascript-lit/listas/questao01.js
+++ estudos-javascript/lista-javascript-lit/listas/questao01.js
@@ -1,2 +1,11 @@
 /* Escreva um programa que receba uma lista de números inteiros e retorne a soma dos elementos da lista */
 
+function somaLista(lista) {
+    let soma = 0;
+
+    for (let i = 0; i < lista.length; i++) soma += lista[i];
+
+    return soma;
+}
+
+console.log(somaLista([1, 2, 3, 4, 5]));
